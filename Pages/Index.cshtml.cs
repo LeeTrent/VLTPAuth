849feac96d1f1f6cccaddae4d65ab4caf98b9fc5
LeeTrent <Lee.Trent.1@gmail.com>
2018-12-17 12:45:17 -0600

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VLTPAuth.Pages
+{
+    public class IndexModel : PageModel
+    {
+        // public void OnGet()
+        // {
+
+        // }
+
+        public IActionResult OnGet() 
+        { 
+          return RedirectToPage("/Account/Login", new { area = "Identity" }); 
+        } 
+
+        // public async Task<IActionResult> OnGetAsync()
+        // {
+        //   // https://stackoverflow.com/questions/51552561/razor-pages-web-app-net-core-2-1-how-to-handle-redirection-from-one-area-pa
+        //   //https://www.learnrazorpages.com/advanced/areas#routing-and-url-generation 
+           
+        //     string url = Url.Page("/Account/Register", new { area = "Identity" });
+        //     return RedirectToPage(url);
+        // }
+    }
+}
